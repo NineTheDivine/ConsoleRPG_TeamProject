ff436250c21cdf2fe46f8f5e26240c591a385e03
Kim Jun Hyeong <kheami35@gmail.com>
2025-07-14 17:33:07 +0900

    feat abstract Entity

--- ConsoleRPG_Team/Program.cs
+++ ConsoleRPG_Team/Program.cs
@@ -1,6 +1,44 @@
-﻿namespace ConsoleRPG_Team
+﻿namespace ConsoleRPG_TeamKJH
 {
-    internal class Program
+    abstract class Entity
+    {
+        protected string name;
+        protected int level;
+        protected int atk;
+        protected int health;
+        protected int maxHealth;
+
+        public abstract void OnTurn();
+
+        public abstract void AtkDiff();
+
+        public abstract void OnDamageDelt();
+
+    }
+
+
+    class Enemy : Entity
+    {
+        private int enemyID;
+        private bool isDead;
+
+
+        public override void OnTurn()
+        {
+            Console.WriteLine("턴");
+        }
+
+        public override void AtkDiff()
+        {
+
+        }
+
+        public override void OnDamageDelt()
+        {
+
+        }
+
+        internal class Program
     {
         static void Main(string[] args)
         {
