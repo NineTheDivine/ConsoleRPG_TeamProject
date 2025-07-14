861d4716a80070566a94b22a79c3e2224cd2bbc0
ntw0926 <1567wkddnjs@gmail.com>
2025-07-14 19:25:27 +0900

    Added base Scene template
    + Added : Abstract class Scene.
    + Added : All scenes needed for the game.

---
+++ ConsoleRPG_Team/Scene/BattleEndScene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG_Team.Scene
+{
+    internal class BattleEndScene : Scene
+    {
+        public BattleEndScene() 
+        {
+            this.inputstream = new string[]
+                {
+                    "0. 다음"
+                };
+        }
+        public override void OnSceneEnter()
+        {
+            AskInput(0, 0);
+        }
+    }
+}
---
+++ ConsoleRPG_Team/Scene/StatusScene.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG_Team.Scene
+{
+    internal class StatusScene : Scene
+    {
+        public StatusScene() 
+        {
+            this.inputstream = new string[]
+                {
+                    "0. 나가기"
+                };
+        }
+        public override void OnSceneEnter()
+        {
+            AskInput(0, 0);
+        }
+    }
+}
