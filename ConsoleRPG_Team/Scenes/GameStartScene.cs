3f7263de2aae13b5fb62ef4fd0de32c1da1069b1
ntw0926 <1567wkddnjs@gmail.com>
2025-07-15 11:23:31 +0900

    Modified Scene Hirearky
    * Modified : "Scene" directory now changed to "Scenes"
    + Added : SceneType Enum to Scene class
    * Modified : OnSceneEnter method now returns SceneType, also added some implements.

---
+++ ConsoleRPG_Team/Scenes/GameStartScene.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG_Team.Scenes
+{
+    internal class GameStartScene : Scene
+    {
+        public GameStartScene() 
+        {
+            this.inputstream = new string[]
+                {
+                    "1. 상태 보기",
+                    "2. 전투 시작",
+                    "0. 게임 종료"
+                };
+        }
+        public override SceneType OnSceneEnter() 
+        {
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine("이제 전투를 시작할 수 있습니다.");
+            int? input = AskInput(0, 2);
+            switch (input)
+            {
+                case 0:
+                    return SceneType.Quit;
+                case 1:
+                    return SceneType.StatusScene;
+                case 2:
+                    return SceneType.BattleScene;
+                default:
+                    Console.WriteLine("Unexpected Behaviour at GameStartScene");
+                    return SceneType.Quit;
+            }
+
+        }
+    }
+}
---
+++ ConsoleRPG_Team/Scenes/StatusScene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG_Team.Scenes
+{
+    internal class StatusScene : Scene
+    {
+        public StatusScene() 
+        {
+            this.inputstream = new string[]
+                {
+                    "0. 나가기"
+                };
+        }
+        public override SceneType OnSceneEnter()
+        {
+            //Need to implement
+
+            //Need to implement End
+            int? input = AskInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    return SceneType.StartScene;
+                default:
+                    Console.WriteLine("Unexpected Behaviour at StatusScene");
+                    return SceneType.Quit;
+            }
+        }
+    }
+}
