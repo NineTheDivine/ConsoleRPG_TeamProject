b7e2b8c340f3ea9c2fba6f9f7f9ba5f9c1394169
ntw0926 <1567wkddnjs@gmail.com>
2025-07-16 17:59:13 +0900

    Quest Bus Implemented
    + Added : QuestEventBus class handles events in the middle
    + Added : Quest class and following structs : Based on QuestID, QuestEventBus will save Actions in the Dictionary.
    + Added : QuestScene to Accept quests
    // On Quest Completion, you can add Subscribe(QuestID) inside the method to call all the published methods //

---
+++ ConsoleRPG_Team/Quests/Quest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG_Team.Quests
+{
+    public enum QuestType
+    {
+        None = -1,
+        Other = 0,
+        BattleWin,
+        SlainEnemy
+
+    };
+
+    public struct QuestID
+    {
+        public QuestType Type;
+        public int ID;
+
+        public QuestID(QuestType T, int id)
+        {
+            this.Type = T;
+            this.ID = id;
+        }
+    };
+
+    public struct QuestInfo
+    {
+        public int finalProgress { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        //public Reward
+        public QuestInfo(int finalprogress, string name, string description)
+        {
+            this.finalProgress = finalprogress;
+            this.name = name;
+            this.description = description;
+        }
+
+
+        public static readonly Dictionary<QuestID, QuestInfo> QUESTCATEGORY = new Dictionary<QuestID, QuestInfo>()
+        {
+            { new QuestID(QuestType.BattleWin, 0) , new QuestInfo(1, "적과의 경험", "전투에서 1회 승리하세요")},
+            { new QuestID(QuestType.BattleWin, 1), new QuestInfo(3, "적 소탕작전", "전투에서 3회 승리하세요") },
+            { new QuestID(QuestType.BattleWin, 2), new QuestInfo(5, "적 말살작전", "전투에서 5회 승리하세요") },
+            { new QuestID(QuestType.SlainEnemy, 0), new QuestInfo(3, "미니언 처치", "미니언 세마리를 잡으세요") },
+            { new QuestID(QuestType.SlainEnemy, 1), new QuestInfo(3, "공허충 처치", "공허충 세마리를 잡으세요") },
+            { new QuestID(QuestType.SlainEnemy , 2), new QuestInfo(3, "대포미니언 처치", "대포미니언 세마리를 잡으세요") }
+        };
+    }
+
+
+    internal class Quest
+    {
+        public QuestID questID { get; private set; }
+        public QuestInfo questInfo {  get; private set; }
+        bool isActive { get; set; } = false;
+        public bool isClear { get; protected set; } = false;
+        public int currentProgress { get; protected set; } = 0;
+
+        Action questAction;
+
+        public Quest()
+        {
+            throw new NotImplementedException();
+        }
+        public Quest(QuestID id)
+        {
+            this.questID = id;
+            if (!QuestInfo.QUESTCATEGORY.ContainsKey(questID))
+                throw new NotImplementedException();
+            this.questInfo = QuestInfo.QUESTCATEGORY[questID];
+            this.isActive = false;
+            this.isClear = false;
+            this.currentProgress = 0;
+            this.questAction = OnProgress;
+        }
+        public void OnQuestSelected()
+        {
+            this.isActive = true;
+            GameManager.activeQuest = this;
+            QuestEventBus.Subscribe(this.questID, this.questAction);
+        }
+        protected void OnProgress()
+        {
+            if (!this.isActive || isClear)
+                return;
+            this.currentProgress++;
+            if (this.currentProgress >= this.questInfo.finalProgress)
+            {
+                this.isClear = true;
+            }
+        }
+
+        protected void RecieveReward()
+        {
+            QuestEventBus.Unsubscribe(this.questID, this.questAction);
+        }
+    }
+}
---
+++ ConsoleRPG_Team/Scenes/QuestScene.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleRPG_Team.Quests;
+
+namespace ConsoleRPG_Team.Scenes
+{
+    internal class QuestScene : Scene
+    {
+        private string[] acceptstream = new string[]
+            {
+                "1. 수락",
+                "2. 거절"
+            };
+        public QuestScene() 
+        {
+            this.inputstream = new string[]
+                {
+                    "0. 이전으로"
+                };
+        }
+        public override SceneType OnSceneEnter()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Quest!!");
+            Console.WriteLine();
+
+            int? input = null;
+            Console.ResetColor();
+            /* Need to be fixed*/
+            do
+            {
+                int questIndex = 1;
+                Quest quest = new Quest(new QuestID(QuestType.BattleWin, 0));
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("{0:2}. ", questIndex.ToString());
+                Console.ResetColor();
+                Console.WriteLine(quest.questInfo.name);
+
+                input = AskInput(0, 1, this.inputstream);
+                if (input != null && input != 0)
+                {
+                    int? acceptInput = null;
+                    do
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Quest!!");
+                        Console.WriteLine();
+                        Console.ResetColor();
+
+                        Console.WriteLine(quest.questInfo.name);
+                        Console.WriteLine();
+                        Console.WriteLine(quest.questInfo.description);
+                        acceptInput = AskInput(1,2, this.acceptstream);
+                    } while (acceptInput == null);
+                    if (acceptInput == 1)
+                    {
+                        Console.WriteLine("{0}을 수락하셨습니다." , quest.questInfo.name);
+                        quest.OnQuestSelected();
+                        input = 0;
+                    }
+                    else
+                        input = null;
+                }
+            } while (input == null);
+
+            return SceneType.StartScene;
+        }
+    }
+}
